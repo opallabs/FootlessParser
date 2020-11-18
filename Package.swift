a80ff1daeb4d356199bcea7e18d15a35a970223e
Mathew Polzin <matt.polzin@gmail.com>
2020-11-18 13:42:38 -0800

    Update Package.swift
    Fix silly mistake in package manifest

--- Package.swift
+++ Package.swift
@@ -1,9 +1,3 @@
-import PackageDescription
-
-let package = Package(
-    name: "FootlessParser"
-)
-
 // swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
