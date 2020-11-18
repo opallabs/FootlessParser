10590af511b91fc3f7a1e20918ac34ff1a3d3d02
Mathew Polzin <matt.polzin@gmail.com>
2020-11-18 13:41:23 -0800

    Update Package.swift
    update package manifest.

--- Package.swift
+++ Package.swift
@@ -3,3 +3,27 @@ import PackageDescription
 let package = Package(
     name: "FootlessParser"
 )
+
+// swift-tools-version:5.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "FootlessParser",
+    products: [
+        .library(
+            name: "FootlessParser",
+            targets: ["FootlessParser"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "FootlessParser",
+            dependencies: [],
+            path: "Sources"),
+        .testTarget(
+            name: "FootlessParserTests",
+            dependencies: ["FootlessParser"]),
+    ]
+)
